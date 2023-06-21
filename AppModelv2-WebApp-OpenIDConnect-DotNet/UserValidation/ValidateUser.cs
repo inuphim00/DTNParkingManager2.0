afeada84bf533883c99b92b386b1f56c31213b17
Seraphim Nombre <Seraphim.Nombre@gmail.com>
2023-06-21 17:24:42 +0800

    Added user setup

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/UserValidation/ValidateUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.UserValidation
+{
+    public class ValidateUser
+    {
+    }
+    public string isFirstTimeUser(string name)
+    {
+        var result = "";
+        return result;
+    }
+}
